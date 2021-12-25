36d33c3bbc11eb280d24e2c52752c66b3f0fc942
FanRende <zxcj04@gmail.com>
2021-12-25 21:54:26 +0800

    Initial Commit

---
+++ AISUdonShavedSnow/AISUdonShavedSnowApp.swift
@@ -0,0 +1,20 @@
+//
+//  AISUdonShavedSnowApp.swift
+//  AISUdonShavedSnow
+//
+//  Created by FanRende on 2021/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AISUdonShavedSnowApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
